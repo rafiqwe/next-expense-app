96489c83ec22680f43601113ad369d8c7f20ac7e
rafiqwe <rmlmanik502@gmail.com>
2025-09-22 21:46:38 +0600

    Created prisma file

---
+++ prisma/migrations/20250922132504_expense_tracker_app/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "cleckUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Record" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "category" TEXT NOT NULL DEFAULT 'Other',
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_cleckUserId_key" ON "public"."User"("cleckUserId");
+
+-- CreateIndex
+CREATE INDEX "Record_userId_idx" ON "public"."Record"("userId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Record" ADD CONSTRAINT "Record_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("cleckUserId") ON DELETE CASCADE ON UPDATE CASCADE;
