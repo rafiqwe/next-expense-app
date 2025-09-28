c9bc927813befb611a1a13041545dd59f9bf3e70
rafiqwe <rmlmanik502@gmail.com>
2025-09-28 10:02:03 +0600

    Created contact provider

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopCotact from '../components/contact/TopCotact'
+import ContactInfo from '../components/contact/ContactInfo'
+
+const ContactPage = () => {
+  return (
+    <div>
+    <TopCotact/>
+    <ContactInfo/>
+    </div>
+  )
+}
+
+export default ContactPage
